147571bf51b20f8022fb533d58dc7bce0b45c120
comsats <comsats@CL6-2.students.comsats.edu.pk>
2021-10-27 14:10:54 +0500

    first project

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
+// You can import from local files
+
+
+// or any pure javascript modules available in npm
+
+const Numbers = () =>{
+  const [getText, setText] = useState("")
+
+const Display = (num) =>{
+  setText(getText+num)
+
+}
+const Clear = () =>{
+  setText("")
+
+}
+  return(
+    
+    
+    
+    <View style={styles.paragraph}>
+    <View style = {{flexDirection:"row",justifyContent: 'space-between' ,padding: '10%' }}>
+       <Button title ="-" onPress={()=>Display("1")}/>
+       <Button title ="-" onPress={()=>Display("1")}/>
+    </View>
+    <View style = {{flexDirection:"row" ,width: '100%', justifyContent: 'space-between', margin: 10}}>
+    <Button title ="-" onPress={()=>Display("1")}/>
+    <Button title ="-" onPress={()=>Display("2")}/>
+    <Button title ="-" onPress={()=>Display("3")}/>
+    </View>
+    <View style = {{flexDirection:"row" ,width: '100%', justifyContent: 'space-between',margin: 10}}>
+    <Button title ="-" onPress={()=>Display("4")}/>
+    <Button title ="-" onPress={()=>Display("5")}/>
+    <Button title ="-" onPress={()=>Display("6")}/>
+    </View>
+    <View style = {{flexDirection:"row" ,width: '100%', justifyContent: 'space-between',margin: 10}}>
+    <Button title ="-" onPress={()=>Display("7")}/>
+    <Button title ="-" onPress={()=>Display("8")}/>
+    <Button title ="-" onPress={()=>Display("9")}/>
+    </View>
+    <View >
+    
+    </View>
+    </View>
+  )
+}
+
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}> 
+      </Text>
+      <Numbers/>    
     </View>
   );
 }
@@ -14,8 +62,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
